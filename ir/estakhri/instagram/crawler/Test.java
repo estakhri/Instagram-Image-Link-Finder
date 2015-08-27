f0056f34179f01799330d53dd32bcea63859e4e7
estakhri <nimaestakhri@gmail.com>
2015-08-28 03:17:29 +0430

    Create Test.java
    This file can be use to fetch instagram images link from a html code of a instagram web page
    
    or give a <div class="-cx-PRIVATE-Photo__clickShield"> with attribute "data-reactid" from html inspector to program as an argument or input and it returns the direct link of that image.

---
+++ ir/estakhri/instagram/crawler/Test.java
@@ -0,0 +1,42 @@
+package ir.estakhri.instagram.crawler;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nima
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+        if (args.length == 1) {
+            String line = args[0];
+            File sourceFile = new File(line);
+            if (sourceFile.isFile()) {
+                System.out.println(ImageLinkFinder.findPhotoFromSource(line));
+            } else {
+
+                line = ImageLinkFinder.getUrl(line);
+                System.out.println(line);
+                StringSelection selection = new StringSelection(line);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(selection, selection);
+            }
+        } else {
+
+            Scanner sc = new Scanner(System.in);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                line = ImageLinkFinder.getUrl(line);
+                System.out.println(line);
+                StringSelection selection = new StringSelection(line);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(selection, selection);
+            }
+        }
+    }
+}
